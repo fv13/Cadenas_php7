0846b86d3c21c61c3cb77f8b0256fdc813fd8183
FV13 <43671750+fv13@users.noreply.github.com>
2018-12-14 12:28:18 +0100

    Add files via upload

---
+++ hexadecimal.php
@@ -0,0 +1,5 @@
+<?php
+require ('../Cadenas_php7/funcion.php');
+
+$hex= funcion::data('hex');
+funcion::hexadecimal($hex);
---
+++ hololapa.php
@@ -0,0 +1,8 @@
+<?php
+require ('../Cadenas_php7/funcion.php');
+$cad = funcion::data('cad');
+funcion::hololapa($cad);
+?>
+
+
+
